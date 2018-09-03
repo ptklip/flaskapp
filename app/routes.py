5725a5480caf20da8b1c7df4f1b4a659e655f1ea
ptklip <ptklip@gmail.com>
2018-09-03 00:34:40 -0400

    Now users are shown on the users page where they belong.

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,10 @@ import records
 @flaskapp.route('/')
 @flaskapp.route('/index')
 def index():
+    return render_template('index.html')
+
+@flaskapp.route('/users')
+def users():
     DATABASE_URL=os.environ['DATABASE_URL']
     db = records.Database(DATABASE_URL)
     sql = """SELECT username, first_name, last_name, email, phone, user_status, start_time 
@@ -14,18 +18,7 @@ def index():
 
     for r in rows:
         colnames = r.keys()
-    return render_template('index.html', recordset=rows, colnames=colnames)
-
-@flaskapp.route('/users', methods=['GET', 'POST'])
-def show_users():
-    DATABASE_URL=os.environ['DATABASE_URL']
-    db = records.Database(DATABASE_URL)
-    sql = """SELECT username, first_name, last_name, email, phone, user_status, start_time 
-            FROM users"""
-    rows = db.query(sql)
-    
-    #rows.export('csv')
-    return render_template('users.html', recordset=rows)
+    return render_template('users.html', recordset=rows, colnames=colnames)
 
 @flaskapp.route('/<name>')
 def hello_name(name):
