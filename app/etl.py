62ede74240ed599697b75d52a8962874b231d231
ptklip <ptklip@gmail.com>
2018-09-05 22:09:38 -0400

    Added data cleanup to the batting stats data processing. Removed parens and other symbols from player name column.

--- app/etl.py
+++ app/etl.py
@@ -10,29 +10,28 @@ from bs4 import BeautifulSoup
 import lxml
 import pandas as pd
 import os
-import records
+from sqlalchemy import create_engine
+import psycopg2 
+import io
 
 DATABASE_URL=os.environ['DATABASE_URL']
-db = records.Database(DATABASE_URL)
 
 def red_sox_batting_stats():
     # Get a page from the web
     url = 'https://www.baseball-reference.com/teams/BOS/2018.shtml'
-    # Save local copy for testing:
-    # url_to_scrape = '2018.shtml'
-
+ 
     response = requests.get(url)
-    # print(response.status_code)
 
     # Process page from the web.
     soup = BeautifulSoup(response.text, 'lxml')
 
-    # Or process local file.
-    # with open('2018.shtml', 'r') as f:
-    #     soup = BeautifulSoup(f, 'lxml')
-
+    # Find the batting stats table.
     table = soup.find('table', id='team_batting')
 
+    # Create a matrix:
+    # For each row, get the text from each cell.
+    # The table has empty cells which must be replaced by something,
+    # e.g. 'null', in order to preserve row length.
     outer_list = []
     for row in table.find_all('tr'):
         inner_list = []
@@ -40,7 +39,8 @@ def red_sox_batting_stats():
             text = cell.text
             inner_list.append(''.join(text) if text else 'null')
         outer_list.append(inner_list)
-        
+
+    # Create a pandas DataFrame from the matrix.   
     df = pd.DataFrame(outer_list)
     df.columns = ['Pos', 'Name', 'Age', 'G', 'PA', 'AB', 'R', 'H', '2B', \
         '3B', 'HR', 'RBI', 'SB', 'CS', 'BB', 'SO', 'BA', 'OBP', 'SLG', 'OPS', \
@@ -49,27 +49,20 @@ def red_sox_batting_stats():
     # Drop rows that do not contain stats.
     df = df[~df['Pos'].isin(['null'])]
     df = df.dropna()
-    # Replace 'null' with 0.
-    df = df.replace('null', 0)
+    # Replace 'null' with 0. Replace (strings in parentheses) with nothing.
+    # Replace '*' with nothing. Replace '#' with nothing.
+    df = df.replace('null', 0).replace(r'\(.*\)', '', regex=True) \
+        .replace(r'\*', '', regex=True).replace(r'\#', '', regex=True)
+    
+    # print(df.to_string()) # Print the DataFrame to the screen.
 
-    # TODO: Remove text in parantheses in Name column.
-    # Example: (10-day dl)
-    # var regExp = /\(([^)]+)\)/;
-
-    print(df.to_string())
-
-    from sqlalchemy import create_engine
-    import psycopg2 
-    import io
-
-    #engine = create_engine('postgres://flaskapp_user:924462@localhost/flaskapp')
+    # Insert the DataFrame into PostgreSQL
     engine = create_engine(DATABASE_URL)
     conn = engine.raw_connection()
     cur = conn.cursor()
     output = io.StringIO()
     df.to_csv(output, sep='\t', header=False, index=True)
     output.seek(0)
-    contents = output.getvalue()
     cur.copy_from(output, 'batting_stats_current_season')
     conn.commit()
 
